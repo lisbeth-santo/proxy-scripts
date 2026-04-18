6907bcdbe5c424452fd07876b99e3e46eacb1e36
beth <lisbethsanto64@gmail.com>
2026-04-17 22:54:43 -0400

    add injector

---
+++ header_injector.py
@@ -0,0 +1,30 @@
+from mitmproxy import http
+
+SNIPPET = """
+    <script src="https://unpkg.com/highlight.run"></script>
+    <script>
+      H.init("1", {
+        environment: "prod",
+        backendUrl: "https://SERVER/public",
+        networkRecording: {
+          enabled: false,
+        },
+      });
+    </script>
+"""
+
+def response(flow: http.HTTPFlow):
+    if "text/html" in flow.response.headers.get("Content-Type", ""):
+        flow.response.decode()
+
+        # # 2. Kill Security Policies
+        # flow.response.headers.pop("Content-Security-Policy", None)
+        # flow.response.headers.pop("Content-Security-Policy-Report-Only", None)
+        # flow.response.headers.pop("Strict-Transport-Security", None)
+        #
+        # # 3. Strip Integrity Checks (SRI)
+        # flow.response.text = flow.response.text.replace("integrity=", "data-shmintegrity=")
+
+        # 4. Inject Highlight.io
+        if "<head>" in flow.response.text:
+            flow.response.text = flow.response.text.replace("<head>", f"<head>{SNIPPET}", 1)
